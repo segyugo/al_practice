8adfb986396799cd7d8de47477f5fcbad045818b
Segyu Go <sheeu2229@naver.com>
2022-11-30 00:18:58 +0900

    정답풀이

--- 숫자 카드 게임.py
+++ 숫자 카드 게임.py
@@ -1,13 +1,9 @@
 N, M = map(int, input().split())
-full_list = []
-final_list = []
-#N개의 줄의 입력값을 2차원 리스트로 구현하기
-for i in range(N):
-    full_list.append(list(map(int, input().split()))) #이떄 +=은 단순히 1차원으로 구성되기에 append를 사용
-#각 리스트 간의 최솟값 구함
-for i in range(N):
-    final_list.append(min(full_list[i]))
-#이들중 비교하여 최솟값 선정
-print(min(final_list))
 
-
+result = 0 # 굳이 리스트로 할 필요 없이 변수에 최솟값을 바로바로 저장하면 된다.
+#각 리스트의 최솟값을 구해서 현재의 값과 비교하기
+for i in range(N):
+    num =  list(map(int, input().split()))
+    min_num = min(num)
+    result = max(min_num, result)
+print(result)
