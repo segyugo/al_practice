c0e12d017defc66466edb7716a8c2688d80c4c68
Segyu Go <sheeu2229@naver.com>
2022-12-03 23:05:35 +0900

    정답풀이

--- 왕실의 나이트.py
+++ 왕실의 나이트.py
@@ -1,25 +1,18 @@
-#좌표를 받음
+#문자열을 받고 이를 좌표로 치환
+
+# 차이점1. 문자열 인덱싱 & ord를 통해 간단하게 좌표로 치환
 a = input()
-
-#문자의 위치를 리스트로 설정
-x_list = ["a","b","c","d","e","f","g","h"]
-y_list = ['1', '2', '3', '4', '5', '6', '7', '8']
-for i in range(8):
-    if x_list[i] in a:
-        x = i+1
-    if y_list[i] in a:
-        y = i+1
-
+x = int(ord(a[0])-ord('a'))+1
+y = int(a[1])
 count = 0
 #좌표를 8개의 케이스별로 움직임
-x_move = [1,-1, 2, 2, 1,-1,-2,-2]
-y_move = [2, 2, 1,-1,-2,-2, 1,-1]
-for i in range(len(x_move)):
-    nx = x + x_move[i]
-    ny = y + y_move[i]
+# 좌표를 하나로 만들어 리스트로 반복문을 돌려 코드가 간단해짐
+move = [(-2, -1),(-2, 1),(2, -1),(2, 1),(-1, -2),(-1, 2),(1, -2),(1, 2)]
+for i in move:
+    nx = x + i[0]
+    ny = y + i[1]
     if nx <=0 or ny <=0 or nx > 8 or ny>8:
         continue
     count += 1
 print(count)
-#벗어나는건 카운트하지 않음
-#출력
+#차이점 1
