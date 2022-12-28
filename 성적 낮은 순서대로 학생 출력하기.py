9043737b775fb31bf212350b6d4bc4da2825f968
segyugo <119419619+segyugo@users.noreply.github.com>
2022-12-28 17:42:35 +0900

    정답

--- 성적 낮은 순서대로 학생 출력하기.py
+++ 성적 낮은 순서대로 학생 출력하기.py
@@ -5,18 +5,14 @@
 def setting(data):
     return data[1]
 
-
-
 N = int(input())
 
 info_list = []
 
 for i in range(N):
-    name, grade = input().split()
-    info = []
-    info.append(name)
-    info.append(grade)
-    info_list.append(info)
+    input_data = input().split()
+    # 리스트도 append할 수 있다.
+    info_list.append((input_data[0], int(input_data[1])))
 
 info_list.sort(key = setting)
 print(info_list)
